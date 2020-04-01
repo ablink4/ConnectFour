c5d48635d8e7ca9a6a777a184a0cb89f9c6e0e50
ablink4 <aaron.blinka@gmail.com>
2020-04-01 06:37:23 -0500

    Fixed the circles for the game board so that they display properly.  Need to clean them up to reduce duplication, etc.  They will need to become their own components, I  think.

--- ConnectFour/ClientApp/src/components/ConnectFour.js
+++ ConnectFour/ClientApp/src/components/ConnectFour.js
@@ -1,5 +1,5 @@
 ﻿import React, { Component } from 'react';
-import '../index.css';
+import './BoardCircle.css';
 
 export class ConnectFour extends Component {
 	displayName = ConnectFour.name
@@ -24,75 +24,85 @@ export class ConnectFour extends Component {
 
 				<p>Select a difficulty level:</p>
 				<input type="radio" id="easy" name="difficulty" value="easy" checked={this.state.currentDifficulty === "easy"} onChange={this.setDifficulty}/>
-				<label for="easy">Easy: The computer selects any free slot at random.</label>
+				<label htmlFor="easy">Easy: The computer selects any free slot at random.</label>
 				<br/>
 
 				<input type="radio" id="medium" name="difficulty" value="medium" checked={this.state.currentDifficulty === "medium"} onChange={this.setDifficulty}/>
-				<label for="easy">Medium: The computer initially choses a strategy at random and sticks to it, regardless of if it stops being the best strategy later in the game.</label>
+				<label htmlFor="easy">Medium: The computer initially choses a strategy at random and sticks to it, regardless of if it stops being the best strategy later in the game.</label>
 				<br/>
 
 				<input type="radio" id="hard" name="difficulty" value="hard" checked={this.state.currentDifficulty === "hard"} onChange={this.setDifficulty}/>
-				<label for="easy">Hard: The computer switches between a variety of strategies to select the best move available based on a set of pre-programmed rules.</label>
+				<label htmlFor="easy">Hard: The computer switches between a variety of strategies to select the best move available based on a set of pre-programmed rules.</label>
 				<br/>
 				<input type="radio" id="learning" name="difficulty" value="learning" checked={this.state.currentDifficulty === "learning"} onChange={this.setDifficulty}/>
-				<label for="easy">Learning Mode: The computer uses an unsupervised learning algorithm and training data from all its previous games to determine its next move.</label>
+				<label htmlFor="easy">Learning Mode: The computer uses an unsupervised learning algorithm and training data from all its previous games to determine its next move.</label>
 				<br />
 
-				{/* still working on getting this to work.  trying to figure out how to get CSS to render in react components.  I think I'm just missing something obvious.  I'll get it. */}
-				<span className="span dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<br />
+				<div className="flex-grid">
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<br />
+				</div>
 
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<br />
+				<div className="flex-grid">
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<br />
+				</div>
 
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<br />
+				<div className="flex-grid">
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<br />
+				</div>
 
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<br />
+				<div className="flex-grid">
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<br />
+				</div>
 
+				<div className="flex-grid">
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<br />
+				</div>
 
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<br />
-
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<span class="dot"></span>
-				<br />
+				<div className="flex-grid">
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<div className="BoardCircle"></div>
+					<br />
+				</div>
 			</div>
 		);
 	}
